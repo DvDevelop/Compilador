5a33ab738fa162c34bc52572c21a26bd57fc9095
David <dvdevelop@gmail.com>
2020-02-22 20:45:04 -0600

    Agregado el contador de lineas y columnas(no agregado al Editor)

---
+++ sabe.py
@@ -0,0 +1,17 @@
+import tkinter as Tk
+
+def countlines(event):
+    (line, c) = map(int, event.widget.index("end-1c").split("."))
+    print(event.widget.index(Tk.INSERT).split("."))
+    print (line, c)
+
+root = Tk.Tk()
+root.geometry("200x200")
+a = Tk.Text(root)
+a.pack()
+bindtags = list(a.bindtags())
+bindtags.insert(2, "custom")
+a.bindtags(tuple(bindtags))
+a.bind_class("custom", "<Key>", countlines)
+
+root.mainloop()
